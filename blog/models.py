750c6247f87ffff22186faeb624eb2ea34bad1eb
Ahmed <ahmedtarekelbahgy@gmail.com>
2025-11-26 18:10:35 +0200

    create the database schema

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,54 @@
 from django.db import models
+from django.conf import settings
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        (0, "Draft"),
+        (1, "Published"),
+    )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, db_index=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = RichTextField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(MPTTModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=2000)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+
+
+class NewsletterSubscriber(models.Model):
+    email = models.EmailField(
+        unique=True, error_messages={"unique": "Already subscribed"}
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    is_subscribed = models.BooleanField(
+        default=False, help_text="Tracks if the auth user wants email updates."
+    )
+    bio = models.TextField(
+        null=True, blank=True, help_text="Optional profile bio for the Author or Users."
+    )
+
+    def __str__(self):
+        return self.username
